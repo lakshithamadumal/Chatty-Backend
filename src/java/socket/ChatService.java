1702724558612a481c2e6f21e5666c9e97444052
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-10-03 10:00:15 +0530

    Update ChatService.java

--- src/java/socket/ChatService.java
+++ src/java/socket/ChatService.java
@@ -9,15 +9,22 @@ import javax.websocket.Session;
  * @author Laky
  */
 public class ChatService {
-
+    
     private static final ConcurrentHashMap<String, Session> SESSIONS = new ConcurrentHashMap<>();
     private static final Gson GSON = new Gson();
-
+    
     public static void register(String userId, Session session) {
         ChatService.SESSIONS.put(userId, session);
     }
-
+    
     public static void unregister(String userId) {
         ChatService.SESSIONS.remove(userId);
+    }
+    
+    public static void sendToUser(String userId, Object payload) {
+        Session WS = ChatService.SESSIONS.get(userId);
+        if (WS != null && WS.isOpen()) {
+            
+        }
     }
 }
