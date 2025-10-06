09276e4002dda236a2229d39f08459a76a70b444
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-10-06 16:56:11 +0530

    uu

--- src/java/socket/ChatService.java
+++ src/java/socket/ChatService.java
@@ -26,7 +26,7 @@ public class ChatService {
 
     private static final ConcurrentHashMap<Integer, Session> SESSIONS = new ConcurrentHashMap<>();
     private static final Gson GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create();
-    public static final String URL = "https://583a35f0ae0b.ngrok-free.app"; // ngrok proxy url
+    public static final String URL = "https://6cf11cdbbe41.ngrok-free.app"; // ngrok proxy url
 
     public static void register(int userId, Session session) {
         SESSIONS.put(userId, session);
